7eabc42cdc0934a617fdaf1b5b84b84a17c8075d
kick0291 <kick0291@gmail.com>
2016-09-29 13:47:29 +0800

    eee

---
+++ ivor_resume/js/main.js
@@ -0,0 +1,40 @@
+
+// button按下錨點滑動進入
+
+    $(function() {
+        $('#ivorb').click(function(){
+          $('html,body').animate({scrollTop:$('#ivor').offset().top},500) ;
+        });
+
+        $('#worksb').click(function(){
+          $('html,body').animate({scrollTop:$('#works').offset().top},500) ;
+        });
+
+        $('#profilesb').click(function(){
+          $('html,body').animate({scrollTop:$('#profiles').offset().top},500) ;
+        });
+
+        $('#contactb').click(function(){
+          $('html,body').animate({scrollTop:$('#contact').offset().top},500);
+        });
+
+        });
+    });
+
+
+// 技能bar捲動
+
+    $(function() {
+      $(window).scroll(function() {
+
+        if($(window).scrollTop() >= $('.skill-wrap').offset().top){
+          $('.bar1-color').delay(1000).css('width', '49vw');
+          $('.bar2-color').delay(1000).css('width', '21vw');
+          $('.bar3-color').delay(1000).css('width', '38vw');
+          $('.bar4-color').delay(1000).css('width', '38vw');
+          $('.bar5-color').delay(1000).css('width', '45vw');
+        }
+      });
+
+  });
+});
